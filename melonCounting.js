3f8dbcb283e027787140ab39827966836dbda233
Jenny <han.jennyjh@outlook.com>
2021-03-17 06:02:20 +0000

    Initial commit

---
+++ melonCounting.js
@@ -0,0 +1,25 @@
+const melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+function count_melons(melons_to_add) {
+    const melon_counts = {}
+    // Creating a diction of melons and it's count under melon_counts
+    for (const melon of melons_to_add) {
+        if (melon_counts[melon]) {
+            melon_counts[melon] += 1; 
+        } else {
+            melon_counts[melon] = 1;
+        }
+
+    }
+    return melon_counts;
+}
